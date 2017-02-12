39d8455a6993e7b0e4652bcb4a4789abe7d217b9
IvanAksionau <vanionov2015@mail.ru>
2017-02-12 05:31:05 +0300

    commit

--- src/by/tc/nb/bean/FindNotesByContentResponse.java
+++ src/by/tc/nb/bean/FindNotesByContentResponse.java
@@ -1,6 +1,5 @@
 package by.tc.nb.bean;
 
-import by.tc.nb.bean.entity.Note;
 
 import java.util.List;
 
--- src/by/tc/nb/command/impl/FindNotesByContent.java
+++ src/by/tc/nb/command/impl/FindNotesByContent.java
@@ -4,7 +4,6 @@ import by.tc.nb.bean.FindNotesByContentRequest;
 import by.tc.nb.bean.FindNotesByContentResponse;
 import by.tc.nb.bean.Request;
 import by.tc.nb.bean.Response;
-import by.tc.nb.bean.entity.Note;
 import by.tc.nb.command.Command;
 import by.tc.nb.command.exception.CommandException;
 import by.tc.nb.service.NoteBookService;
